97fb6d62e7494b8d5e7300be6ba36048f4664a6e
João Paulo Lethier <jplethier@gmail.com>
2021-11-28 18:28:14 -0300

    adds methods and relationship to model

--- app/models/teachers_statistic.rb
+++ app/models/teachers_statistic.rb
@@ -1,4 +1,6 @@
 class TeachersStatistic < ApplicationRecord
+  belongs_to :teacher
+
   # this isn't strictly necessary, but it will prevent
   # rails from calling save, which would fail anyway.
   def readonly?
@@ -7,5 +9,13 @@ class TeachersStatistic < ApplicationRecord
 
   def self.refresh
     Scenic.database.refresh_materialized_view(table_name, concurrently: true, cascade: false)
+  end
+
+  def students_full?
+    total_students > 50
+  end
+
+  def classes_full?
+    total_classes > 3
   end
 end
